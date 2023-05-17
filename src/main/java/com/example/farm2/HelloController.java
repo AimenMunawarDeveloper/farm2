b5ca9ba13ae1666248f775ee73b33608e6fd59a2
Aimen Munawar <90191183+AimenMunawarDeveloper@users.noreply.github.com>
2023-05-17 21:16:43 +0500

    farm> practice
    farm> practice

--- src/main/java/com/example/farm2/HelloController.java
+++ src/main/java/com/example/farm2/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.farm2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    Animal cow = new Animal("Gow Matta", 34.4,23.4,7);
     @FXML
     private Label welcomeText;
+    public Button sampleButton;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText(cow.printAnimal());
     }
 }
