0c6f98e3968d3d06a7b793a880adba540444653d
Aimen Munawar <90191183+AimenMunawarDeveloper@users.noreply.github.com>
2023-05-19 01:34:41 +0500

    fix

---
+++ src/main/java/com/example/farm2/FarmModel.java
@@ -0,0 +1,4 @@
+package com.example.farm2;
+
+public class FarmModel {
+}
--- src/main/java/com/example/farm2/HelloController.java
+++ src/main/java/com/example/farm2/HelloController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    Animal cow = new Animal("Gow Matta", 34.4,23.4,7);
+    Animal cow = new Animal("Cow", 34.4,23.4,7);
     @FXML
     private Label welcomeText;
     public Button sampleButton;
